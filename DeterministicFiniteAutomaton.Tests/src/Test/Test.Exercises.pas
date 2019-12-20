86327f44b0c7ed8d2af604577d1f3ea5b20134ff
Douglas Mezuraro <douglasmez@gmail.com>
2019-12-20 11:33:41 -0300

    Teste unitário do exercício 7 da lista

--- DeterministicFiniteAutomaton.Tests/src/Test/Test.Exercises.pas
+++ DeterministicFiniteAutomaton.Tests/src/Test/Test.Exercises.pas
@@ -13,8 +13,20 @@ type
     procedure SetUp; override;
     procedure TearDown; override;
   published
+    /// <summary>
+    ///   L5 = {a^n/n>0}
+    /// </summary>
     procedure ExerciseFive;
+
+    /// <summary>
+    ///   L6 = {a^n/n>0,n é ímpar}
+    /// </summary>
     procedure ExerciseSix;
+
+    /// <summary>
+    ///   L7 = {a(b^n)a/n>=0,n é par}
+    /// </summary>
+    procedure ExerciseSeven;
   end;
 
 implementation
@@ -65,6 +77,30 @@ begin
 
   FAutomaton.Transitions.Add(TTransition.Create('q0', 'a', 'q1'));
   FAutomaton.Transitions.Add(TTransition.Create('q1', 'a', 'q0'));
+
+  for Word in MustAccept do
+    CheckTrue(FAutomaton.Accept(Word));
+
+  for Word in MustReject do
+    CheckFalse(FAutomaton.Accept(Word));
+end;
+
+procedure TExercisesTest.ExerciseSeven;
+const
+  MustAccept: TArray<TWord> = ['aa', 'abba', 'abbbba', 'abbbbbba', 'abbbbbbbba', 'abbbbbbbbbba'];
+  MustReject: TArray<TWord> = ['', 'ʎ', 'aba', 'abbba', 'abbbbba', 'abbbbbbba', 'abbbbbbbbba'];
+var
+  Word: TWord;
+begin
+  FAutomaton.Symbols := ['a', 'b'];
+  FAutomaton.States := ['q0', 'q1', 'q2', 'q3'];
+  FAutomaton.InitialState := 'q0';
+  FAutomaton.FinalStates := ['q3'];
+
+  FAutomaton.Transitions.Add(TTransition.Create('q0', 'a', 'q1'));
+  FAutomaton.Transitions.Add(TTransition.Create('q1', 'a', 'q3'));
+  FAutomaton.Transitions.Add(TTransition.Create('q1', 'b', 'q2'));
+  FAutomaton.Transitions.Add(TTransition.Create('q2', 'b', 'q1'));
 
   for Word in MustAccept do
     CheckTrue(FAutomaton.Accept(Word));
